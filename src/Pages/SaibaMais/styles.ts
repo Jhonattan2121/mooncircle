a37aab6bb7120563a7461f226f384de7291a219e
jhonattan ferri <jhoontutors@gmail.com>
2023-08-28 21:28:02 -0300

    responsivo e mudancas no saiba mais

--- src/Pages/SaibaMais/styles.ts
+++ src/Pages/SaibaMais/styles.ts
@@ -2,45 +2,48 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   position: relative;
-  width: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   display: flex;
+  width: 59%;
+  height: 73%;
+  top: -17rem;
+  left: -31rem;
+  gap: 8px;
 
   img {
     width: 600px;
-    height: 600px;
+    height: 500px;
     display: flex;
     position: relative;
-    top: -25rem;
-    left: -17rem;
+    top: -4rem;
+    left: 10rem;
   }
 
   .Content {
     display: flex;
     position: relative;
-    top: -56rem;
-    left: -9rem;
+    top: -37rem;
+    left: -5rem;
     flex-direction: column;
     align-items: center;
     box-sizing: border-box;
-    width: 601px;
+    width: 712px;
     height: 542px;
+    gap: 2rem;
   }
 
   .Frame {
     width: 100%;
     max-width: 800px;
     padding: 0px;
-
     color: #ffffff;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-
-    margin-right: -47rem;
+    margin-right: -49rem;
 
     h1 {
-      font-size: 24px;
+      font-size: 29px;
       font-weight: 600;
       margin-bottom: 1rem;
     }
@@ -51,54 +54,63 @@ export const Container = styled.div`
     }
   }
 
-  @media (max-width: 600px) {
-    position: relative;
+  @media (max-width: 425px) {
     width: 100%;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    display: flex;
 
     img {
       width: 200px;
       height: 200px;
       display: flex;
       position: relative;
-      top: -18rem;
-      left: 1rem;
+      top: 2rem;
+      left: 32rem;
     }
 
     .Content {
-      display: flex;
-      position: relative;
-      top: -18rem;
-      left: -23rem;
-      flex-direction: column;
-      align-items: center;
-      box-sizing: border-box;
+      top: 3rem;
+      left: -15rem;
       width: 318px;
       height: 542px;
     }
 
     .Frame {
-      width: 100%;
-      max-width: 800px;
-      padding: 0px;
-
-      color: #ffffff;
-      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-
       margin-right: -47rem;
 
       h1 {
         font-size: 24px;
-        font-weight: 600;
         margin-bottom: 1rem;
       }
+    }
+  }
 
-      p {
-        font-size: 16px;
-        line-height: 1.5;
+  @media (min-width: 426px) and (max-width: 1440px) {
+    /* Estilos específicos para dispositivos com largura entre 426px e 1440px */
+
+    width: 80%;
+    top: -10rem;
+    left: -16rem;
+
+    img {
+      width: 400px;
+      height: 350px;
+      top: 0;
+      left: 0;
+    }
+
+    .Content {
+      top: -20rem;
+      left: -2rem;
+      width: 500px;
+    }
+
+    .Frame {
+      margin-right: -40rem;
+
+      h1 {
+        font-size: 26px;
       }
     }
   }
